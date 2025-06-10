fc6fe2cf98392b0aba2f0d8de2f16d2f8a8e4749
Alex-hpfirefly <alexanderbaekey@gmail.com>
2025-06-10 12:14:55 -0400

    Added automatic lattice parameter setting with cif import

--- python/intensities.py
+++ python/intensities.py
@@ -3,9 +3,60 @@ from java.lang import Runtime
 from javax.swing import JFileChooser
 import java.io.BufferedReader as BufferedReader
 import java.io.InputStreamReader as InputStreamReader
-#import pyepics
 import subprocess
 from org.csstudio.display.builder.runtime.script import PVUtil
+import re
+
+#def parse_cif_lattice_params(cif_path):
+#    lattice = {}
+#    with open(cif_path, 'r') as f:
+#        for line in f:
+#            line = line.strip()
+#            if line.startswith('_cell_length_a'):
+#                lattice['a'] = float(line.split()[1])
+#            elif line.startswith('_cell_length_b'):
+#                lattice['b'] = float(line.split()[1])
+#            elif line.startswith('_cell_length_c'):
+#                lattice['c'] = float(line.split()[1])
+#            elif line.startswith('_cell_angle_alpha'):
+#                lattice['alpha'] = float(line.split()[1])
+#            elif line.startswith('_cell_angle_beta'):
+#                lattice['beta'] = float(line.split()[1])
+#            elif line.startswith('_cell_angle_gamma'):
+#                lattice['gamma'] = float(line.split()[1])
+#    return lattice
+
+def parse_cif_lattice_params(cif_path):
+    lattice = {}
+    pattern = re.compile(r"([-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?)(?:\(\d+\))?")
+    with open(cif_path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith('_cell_length_a'):
+                match = pattern.search(line)
+                if match:
+                    lattice['a'] = float(match.group(1))
+            elif line.startswith('_cell_length_b'):
+                match = pattern.search(line)
+                if match:
+                    lattice['b'] = float(match.group(1))
+            elif line.startswith('_cell_length_c'):
+                match = pattern.search(line)
+                if match:
+                    lattice['c'] = float(match.group(1))
+            elif line.startswith('_cell_angle_alpha'):
+                match = pattern.search(line)
+                if match:
+                    lattice['alpha'] = float(match.group(1))
+            elif line.startswith('_cell_angle_beta'):
+                match = pattern.search(line)
+                if match:
+                    lattice['beta'] = float(match.group(1))
+            elif line.startswith('_cell_angle_gamma'):
+                match = pattern.search(line)
+                if match:
+                    lattice['gamma'] = float(match.group(1))
+    return lattice
 
 
 # Embedded python script
@@ -13,26 +64,10 @@ from org.csstudio.display.builder.runtime.script import PVUtil
 #print 'Hello'
 # widget.setPropertyValue('text', PVUtil.getString(pvs[0]))
 
-
 #print(dir(ScriptUtil))
-
-#pv = ScriptUtil.getPV("loc://abtest:ioc-hkl:intensities")
-#pv.write("Waiting for intensities...")
-
+#print(dir(PVUtil))
 
 #TODO For now, I'm pasting this script into the "import sample .cif" action script in CSS
-
-#TODO get wavelength from IOC with pyepics, feed into cif2hkl, for now
-#wavelength=0.4
-wlenpv = PVUtil.createPV("abtest:ioc-hkl:wlen_RBV", 1000)
-#wlenpv_info = wlenpv.read()
-#wavelength = PVUtil.getDouble(wlenpv_info)
-wavelength = PVUtil.getDouble(wlenpv)
-
-#TODO if wavelength value is entered in box but "Set Wavelength & Sample Lattice" isn't pressed and assigned to the underlying hkl smaple, the resulting intensities from cif2hkl will not be correct
-
-cif2hkl_bin = '/usr/bin/cif2hkl'
-
 
 chooser = JFileChooser()
 chooser.setDialogTitle("Select a CIF file")
@@ -45,7 +80,21 @@ if result == JFileChooser.APPROVE_OPTION:
 
     print(cif_path)
     print(hkl_path)
-    #cmd = [cif2hkl_bin, '--mode', 'NUC', '--lambda', str(wavelength), '--xtal', cif_path, '--out', hkl_path]
+
+    ##### parse .cif and set lattice parameters to IOC #####
+    lattice = parse_cif_lattice_params(cif_path)
+    PVUtil.writePV("abtest:ioc-hkl:latt_a", lattice['a'], 1000)
+    PVUtil.writePV("abtest:ioc-hkl:latt_b", lattice['b'], 1000)
+    PVUtil.writePV("abtest:ioc-hkl:latt_c", lattice['c'], 1000)
+    PVUtil.writePV("abtest:ioc-hkl:latt_alpha", lattice['alpha'], 1000)
+    PVUtil.writePV("abtest:ioc-hkl:latt_beta", lattice['beta'], 1000)
+    PVUtil.writePV("abtest:ioc-hkl:latt_gamma", lattice['gamma'], 1000)
+
+    ##### scattering intensities calculation #####
+    cif2hkl_bin = '/usr/bin/cif2hkl'
+    wlenpv = PVUtil.createPV("abtest:ioc-hkl:wlen_RBV", 1000)
+    wavelength = PVUtil.getDouble(wlenpv)
+    #TODO if wavelength value is entered in box but "Set Wavelength & Sample Lattice" isn't pressed and assigned to the underlying hkl smaple, the resulting intensities from cif2hkl will not be correct
     cmd = [cif2hkl_bin, '--mode', 'NUC', '--out', hkl_path, '--lambda', str(wavelength), '--xtal', cif_path]
     #cmd = [cif2hkl_bin, '--xtal', cif_path]
     print("cmd:", cmd)
@@ -56,45 +105,31 @@ if result == JFileChooser.APPROVE_OPTION:
         print("cif2hkl stderr:\n", err)
     except Exception as e:
         print("Exception running cif2hkl:", e)
-
-
     try:
         with open(hkl_path, "r") as f:
             lines = f.readlines()
-
         intensity_lines = []
         found_data_start = False
-
         for line in lines:
             line = line.strip()
-
             if not line:
                 continue
-
-            # Wait until we hit the reflection header
             if line.startswith("# H") and "|Fc|^2" in line:
                 found_data_start = True
                 continue
-
-            # Skip all lines before the reflection table
             if not found_data_start or line.startswith("#"):
                 continue
-
             parts = line.split()
             if len(parts) >= 6:
                 h, k, l, mult, d, intensity = parts[:6]
                 intensity_lines.append("%3s %3s %3s   %12s" % (h, k, l, intensity))
-
             if len(intensity_lines) >= 50:
                 break
-
     except Exception as e:
         intensity_lines = ["Error: " + str(e)]
-        
     output = "\n".join(intensity_lines)
     print("Parsed reflections:\n", output)
-
-    # Push to local PV for display
+    # local PV does not remain when switching tabs
     #ScriptUtil.getPVByName(widget, "loc://abtest:ioc-hkl:intensities").write(output)
     #PVUtil.writePV("loc://abtest:ioc-hkl:intensities", output, 1000)
     PVUtil.writePV("abtest:ioc-hkl:intensities", output, 1000)
